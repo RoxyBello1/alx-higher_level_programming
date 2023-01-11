a2f00b5608899d41304f1d1724694e8fb79b6cc6
Abigail Iliya Bello <abigalbello2020@gmail.com>
2023-01-11 12:15:01 -0800

    9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_d = {}
+    multiple = {}
     for i in a_dictionary:
-        new_d[i] = a_dictionary[i] * 2
-        return new_d
+        multiple[i] = a_dictionary[i] * 2
+        return multiple
